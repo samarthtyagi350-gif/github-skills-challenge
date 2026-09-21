35caa23129c83f43a9bb2ea84600a8ea22d8709b
samarth_tyagi <samarthtyagi350@gmail.com>
2026-09-21 05:25:07 +0000

    No error in anomaly_detector.py

--- src/anomaly_detector.py
+++ src/anomaly_detector.py
@@ -10,7 +10,6 @@ class AnomalyDetector:
         self.response_time_threshold = response_time_threshold
         self.cpu_threshold = cpu_threshold
         self.memory_threshold = memory_threshold
-        
 
     def detect(self, record):
         reasons = []
